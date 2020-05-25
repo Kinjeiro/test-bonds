8a398a8130d32537ceb3846d6f8d820210dacf31
Andrey Kuzmin <kinjeiro@gmail.com>
2020-05-25 12:39:10 +0300

    bug(task-share): - split tests to describe structure

--- src/modules/module-task-share/task-shares-of-array.test.js
+++ src/modules/module-task-share/task-shares-of-array.test.js
@@ -1,30 +1,36 @@
 import taskCalculatedShare, { getMaxArrayLengthByMaxTime } from './task-shares-of-array';
 
-test('task1 test', () => {
-  const INNER_ARRAY = [
-    '1.5',
-    '3',
-    '6',
-    '1.5'
-  ];
-  const RESULT_ARRAY = [
-    '12.500',
-    '25.000',
-    '50.000',
-    '12.500'
-  ];
+describe('taskCalculatedShare', () => {
+  test('work correct with border conditions', () => {
+    expect(taskCalculatedShare([])).toEqual([]);
+    expect(taskCalculatedShare).toThrow();
+  });
 
-  // Border conditions
-  expect(taskCalculatedShare([])).toEqual([]);
-  expect(taskCalculatedShare).toThrow();
+  test('return correct shares', () => {
+    expect(taskCalculatedShare([
+      '1.5',
+      '3',
+      '6',
+      '1.5'
+    ])).toEqual([
+      '12.500',
+      '25.000',
+      '50.000',
+      '12.500'
+    ]);
+  })
+});
 
-  expect(taskCalculatedShare(INNER_ARRAY)).toEqual(RESULT_ARRAY);
-
-  const MAX_TIME = 5000;
-  const length = getMaxArrayLengthByMaxTime(MAX_TIME, 2000000);
-  const testArray = Array.from({ length }, () => `${(Math.random()*10).toFixed(1)}`);
-  const t0 = performance.now();
-  taskCalculatedShare(testArray);
-  const t1 = performance.now();
-  expect(t1-t0).toBeLessThan(MAX_TIME);
+describe('getMaxArrayLengthByMaxTime', () => {
+  // skip because in project run auto test when release new version
+  // test('should return the number of elements from which there will be shares up to 5 seconds', () => {
+  test.skip('should return the number of elements from which there will be shares up to 5 seconds', () => {
+    const MAX_TIME = 5000;
+    const length = getMaxArrayLengthByMaxTime(MAX_TIME, 2000000);
+    const testArray = Array.from({ length }, () => `${(Math.random()*10).toFixed(1)}`);
+    const t0 = performance.now();
+    taskCalculatedShare(testArray);
+    const t1 = performance.now();
+    expect(t1-t0).toBeLessThan(MAX_TIME);
+  })
 });
